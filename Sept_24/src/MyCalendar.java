4e84e5384ec39087c34ebffe8ed55eebb7d0dc95
Rajeev Kumar <rajeevjewar2@gmail.com>
2024-09-29 18:09:21 +0530

    My Calender I problem is solved

--- Sept_24/src/MyCalendar.java
+++ Sept_24/src/MyCalendar.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.TreeMap;
 
 public class MyCalendar {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+    TreeMap<Integer, Integer> map;
+    public MyCalendar() {
+        map = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        Integer prevVal = map.lowerKey(end);
+        if(prevVal != null && start <= map.get(prevVal)-1){
+            return false;
+        }
+        map.put(start, end);
+        return true;
+    }
 }
