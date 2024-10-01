112c9588cdeb398261a695b2d80019c676af9965
Rajeev Kumar <rajeevjewar2@gmail.com>
2024-10-01 16:24:09 +0530

    Check if array pairs are disible by k problem is solved

---
+++ Oct_24/src/DivisibleArrayPairs.java
@@ -0,0 +1,27 @@
+// Problem: 1497. Check If Array Pairs Are Divisible by k
+// Date: 01/10/24
+
+public class DivisibleArrayPairs {
+    public static void main(String[] args) {
+        int[] arr =  {1,2,3,4,5,10,6,7,8,9};
+        int k = 5;
+        System.out.println(canArrange(arr, k));
+    }
+    static boolean canArrange(int[] arr, int k) {
+        int[] map = new int[k];
+        for(int element: arr){
+            int rem = ((element % k) + k) % k;
+            map[rem]++;
+        }
+        if(map[0]%2 !=0){
+            return false;
+        }
+        for(int rem =1; rem<=k/2; rem++){
+            int comp = k - rem;
+            if(map[rem]!= map[comp]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
